513c7f80858a2abb6994c4d83acbb9972155e11b
zee-german <franz.lutzenberger@gmail.com>
2017-07-19 15:31:51 -0400

    working on the topics controller

--- web/router.ex
+++ web/router.ex
@@ -16,9 +16,13 @@ defmodule Discuss.Router do
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "/", TopicController, :index
+
+    # get "/topics", TopicController, :index
+    post "/topics", TopicController, :create
 
     get "/topics/new", TopicController, :new
+
   end
 
   # Other scopes may use custom stacks.
