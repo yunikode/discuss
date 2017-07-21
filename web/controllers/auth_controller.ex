649659efa76efeb16813e9083d69fa8c201d4aa3
zee-german <franz.lutzenberger@gmail.com>
2017-07-21 19:35:42 -0400

    started auth_controller

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -2,11 +2,19 @@ defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug Ueberauth
 
-  def callback(conn, params) do
-    IO.puts "+++++"
-    IO.inspect(conn.assigns)
-    IO.puts "+++++"
-    IO.inspect(params)
-    IO.puts "+++++"
+  alias Discuss.User
+
+  def callback(%{assigns: %{ueberauth_auth: auth}} = auth, params) do
+    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+    changeset = User.changeset(%User{}, user_params)
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
   end
 end
